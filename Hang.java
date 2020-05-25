7abf5eec3a48bc91fa131d163dba06af50d172ce
Hesam Sadeghian <65884061+HammerTwo1@users.noreply.github.com>
2020-05-25 03:19:44 -0500

    The changes are just tests.

--- Hang.java
+++ Hang.java
@@ -1,5 +1,19 @@
 public class Hang {
     public static void main(String[] args) {
         
+        System.out.print(word("fire"));
+        System.out.print("\n___");
+        
+    }
+
+    public static char word(String w){
+
+        char ran = 'f';
+
+        return ran;
+    }
+
+    public static void body(){
+
     }
 }
