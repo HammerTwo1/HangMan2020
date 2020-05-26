ac6a1d788af0b7602fb3fd0f8e99cb126c031c7c
Hesam Sadeghian <65884061+HammerTwo1@users.noreply.github.com>
2020-05-26 00:32:14 -0500

    added rounds

--- Hang.java
+++ Hang.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+import java.util.Random;
 public class Hang {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Your Name: ");
+        String player = input.nextLine();
+
+        System.out.println("Round1");
+
         
+
         System.out.print(word("fire"));
         System.out.print("\n___");
         
