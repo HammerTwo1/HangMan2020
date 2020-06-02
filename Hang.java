1cd3c52b9574259e8ac7a2c5e8ddbfee2506b95a
Hesam Sadeghian <65884061+HammerTwo1@users.noreply.github.com>
2020-06-02 04:40:36 -0500

    CC & added round

--- Hang.java
+++ Hang.java
@@ -4,10 +4,11 @@ public class Hang {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter Your Social Security Number: ");
+        System.out.print("Enter Your credit card: ");
         String player = input.nextLine();
 
         System.out.println("Round1");
+        System.out.println("Round2 ");
 
         System.out.print(word("fire"));
         System.out.print("\n___");
