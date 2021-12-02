52c34bdb63aa80c69bca36b179c3567cc3994f6b
Hesam <hesamsa45@gmail.com>
2021-12-02 14:49:53 -0600

    change

--- Hang.java
+++ Hang.java
@@ -3,10 +3,10 @@ import java.io.*;
 
 public class Hang {
     public static void main(String[] args) {
-        // Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        // System.out.print("Chose a letter: ");
-        // String player = input.nextLine();
+        System.out.print("Chose a letter: ");
+        char letter = input.next().charAt(0);
 
         
         ArrayList<String> words = new ArrayList<>();
@@ -23,7 +23,13 @@ public class Hang {
             System.out.println("no file not found: " + fnf);
         } 
         
-        System.out.println(words.get(rand(words.size())));
+        char[] charecter = new char[10];
+
+        String randomSelectedWord = words.get(rand(words.size()));
+
+        for(int i = 0; i < randomSelectedWord.length(); i++){
+            charecter[i] = randomSelectedWord.charAt(i);
+        }
     }
 
     public static int rand(int numWords){
